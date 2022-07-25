4f220d385fb7e6c882534257ee703a883b681922
pasupathisakthivel <pasupathi.sakthiuvel@ascendas.in>
2022-07-25 18:04:13 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+import Layout from './pasupathi/Layout';
+//import Nav from './pasupathi/Nav'; 
+
+// import Slide from './pasupathi/Slide';
+//import Position from "./pasupathi/Position";
+//import "./pasupathi/style.css";
+function App() {
+  return (
+    <div className="App">
+       {/* <Nav/>
+       <Slide/> 
+     
+    <Position/>  */}
+    <Layout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MenuBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function MenuBar() {
+  return (
+    <div>MenuBar
+        <h2></h2>
+        
+
+
+
+
+        <div>
+
+
+
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default MenuBar
